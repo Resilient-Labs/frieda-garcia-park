de8b0c77e8da2c3de8e5ebbeccdfea3b006cd9a3
arangodavid <arango.david8@gmail.com>
2017-04-18 20:16:50 -0400

    testing footer

--- site/web/app/themes/sage/templates/footer.php
+++ site/web/app/themes/sage/templates/footer.php
@@ -1,5 +1,52 @@
-<footer class="content-info">
+<!--<footer class="content-info">
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
+    <p>Hello from the footer</p>
   </div>
+</footer>-->
+<footer class="content-info">
+  <section>
+    <h2>Looking to Host an Event at Frieda Garcia Park?</h2>
+    <p>
+      Please reach out if you would like to host a public event at Frieda
+      Garcia Park. There may be an opportunity for the friends of Frieda
+      Garcia Park to help underwrite the costs, at our discretion.
+    </p>
+    <a href="#">
+      <button>Host an Event</button>
+    </a>
+  </section>
+  <section>
+    <h2>Visit the Park:</h2>
+    <ul>
+      <li>38 Stanhope Street</li>
+      <li>Boston, Massachussetts 02116</li>
+      <li>281-330-8004</li>
+      <li>contactme@gmail.com</li>
+    </ul>
+  </section>
+  <section>
+    <h2>Site Map</h2>
+    <ul>
+      <a href="index.html"><li>Park History</li></a>
+      <a href="#"><li>About Frieda</li></a>
+      <a href="#"><li>Get Involved</li></a>
+      <a href="thirdpage.html"><li>Host an Event</li></a>
+      <a href="secondpage.html"><li>Blog</li></a>
+    </ul>
+  </section>
+  <section class="copyRight">
+    <small>Friends of Frieda Garcia Park &copy; 2017. All Rights Reserved</small>
+    <section class="social">
+      <a href="#">
+        <i class="fa fa-facebook fa-2x fLogo" aria-hidden="true"></i>
+      </a>
+      <a href="#">
+        <i class="fa fa-twitter fa-2x tLogo" aria-hidden="true"></i>
+      </a>
+      <a href="#">
+        <i class="fa fa-instagram fa-2x" aria-hidden="true"></i>
+      </a>
+    </section>
+  </section>
 </footer>
