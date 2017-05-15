885faf345b64fe73a7288f50bdce857ecdd9b36c
resilientcoders3 <resilientcoders3@resilientcoders3s-MacBook-Pro.local>
2017-05-15 18:33:50 -0400

    template modification

--- site/web/app/themes/sage/templates/content.php
+++ site/web/app/themes/sage/templates/content.php
@@ -1,9 +1,12 @@
-<article <?php post_class(); ?>>
-  <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php get_template_part('templates/entry-meta'); ?>
-  </header>
-  <div class="entry-summary">
-    <?php the_excerpt(); ?>
+<section class="blog-post-row">
+  <div class="img-wrap">
+      <img src="http://www.n3rdbomber.com/wp-content/uploads/2015/03/Metroid.jpg"/>
   </div>
-</article>
+  <article <?php post_class(); ?>>
+      <div class="content entry-summary clearfix">
+        <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <?php get_template_part('templates/entry-meta'); ?>
+        <?php the_excerpt(); ?>
+    </div>
+  </article>
+</section>
