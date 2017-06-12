02e9b35defbce186b8392fe64823ab4a0d96b4c4
Corriger <rsmoraldo2@gmail.com>
2017-06-12 13:45:17 -0400

    css refactoring

--- site/web/app/themes/sage/templates/template-home.php
+++ site/web/app/themes/sage/templates/template-home.php
@@ -14,7 +14,7 @@
     <article class="content">
       <h2><?php echo get_field('home-top-title');?></h2>
       <?php echo get_field('home-top-body');?>
-      <a href="http://frieda-garcia-park.com/about/"><?php echo get_field('home-top-button');?><i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+      <a href="http://frieda-garcia-park.com/about/"><?php echo get_field('home-top-button');?>&nbsp;<i class="fa fa-long-arrow-right" aria-hidden="true"></i>
       </a>
     </article>
   </section>
@@ -25,7 +25,7 @@
     <article class="content">
       <h2><?php echo get_field('home-middle-title');?></h2>
       <?php echo get_field('home-middle-body');?>
-      <a href="http://frieda-garcia-park.com/get-involved/"><?php echo get_field('home-middle-button');?><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+      <a href="http://frieda-garcia-park.com/get-involved/"><?php echo get_field('home-middle-button');?>&nbsp;<i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
     </article>
   </section>
   <section class="home row">
@@ -35,7 +35,7 @@
     <article class="content">
       <h2><?php echo get_field('home-bottom-title');?></h2>
       <?php echo get_field('home-bottom-body');?>
-      <a href="http://frieda-garcia-park.com/host-an-event/"><?php echo get_field('home-bottom-button');?><i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+      <a href="http://frieda-garcia-park.com/host-an-event/"><?php echo get_field('home-bottom-button');?>&nbsp;<i class="fa fa-long-arrow-right" aria-hidden="true"></i>
       </a>
     </article>
   </section>
