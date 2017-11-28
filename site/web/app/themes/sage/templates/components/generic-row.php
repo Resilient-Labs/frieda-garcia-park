e60f784786cee055966d26d37a00875c38fd05fb
Corriger <rsmoraldo2@gmail.com>
2017-11-28 16:00:44 -0500

    templated homepage

---
+++ site/web/app/themes/sage/templates/components/generic-row.php
@@ -0,0 +1,32 @@
+<section class="home generic-row">
+  <figure class="generic-row-media">
+    <img src="<?php echo get_field('home-top-image');?>">
+  </figure>
+  <article class="content">
+    <h2><?php echo get_field('home-top-title');?></h2>
+    <?php echo get_field('home-top-body');?>
+    <a href="<?php echo get_field('home-top-button');?>"><?php echo get_field('home-top-button-cta');?>&nbsp;<i class="fa fa-long-argeneric-row-right" aria-hidden="true"></i>
+    </a>
+  </article>
+</section>
+<section class="home generic-row">
+  <figure class="generic-row-media">
+    <img src="<?php echo get_field('home-middle-image');?>">
+  </figure>
+  <article class="content">
+    <h2><?php echo get_field('home-middle-title');?></h2>
+    <?php echo get_field('home-middle-body');?>
+  <a href="<?php echo get_field('home-middle-button');?>"><?php echo get_field('home-middle-button-cta');?>&nbsp;<i class="fa fa-long-argeneric-row-right" aria-hidden="true"></i></a>
+  </article>
+</section>
+<section class="home generic-row">
+  <figure class="generic-row-media">
+    <img src="<?php echo get_field('home-bottom-image');?>">
+  </figure>
+  <article class="content">
+    <h2><?php echo get_field('home-bottom-title');?></h2>
+    <?php echo get_field('home-bottom-body');?>
+    <a href="<?php echo get_field('home-bottom-button');?>"><?php echo get_field('home-bottom-button-cta');?>&nbsp;<i class="fa fa-long-argeneric-row-right" aria-hidden="true"></i>
+    </a>
+  </article>
+</section>
