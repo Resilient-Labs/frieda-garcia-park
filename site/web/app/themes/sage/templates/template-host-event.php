d94ac6bddf4d1e2d940e6a64301c3a2da7d38c96
Corriger <rsmoraldo2@gmail.com>
2017-11-30 17:32:27 -0500

    modded host event template

--- site/web/app/themes/sage/templates/template-host-event.php
+++ site/web/app/themes/sage/templates/template-host-event.php
@@ -3,47 +3,5 @@
  * Template Name: Host Event Template
  */
 ?>
-  <section class="generic-row">
-    <section class="content">
-      <h2><?php echo get_field('host-event-title'); ?></h2>
-      <?php echo get_field('host-event-body');?>
-    </section>
-      <figure class="generic-row-media">
-        <img src="<?php echo get_field('host-event-image');?>">
-      </figure>
-  </section>
-  <section class="generic-row">
-    <h2><?php echo get_field('rules-title'); ?></h2>
-    <section class="host-event-card">
-      <figure>
-        <img src="<?php echo get_field('rule-one-image'); ?>">
-      </figure>
-      <div>
-      <?php echo get_field('rule-one'); ?>
-    </div>
-    </section>
-    <section class="host-event-card">
-      <figure>
-        <img src="<?php echo get_field('rule-two-image'); ?>">
-      </figure>
-      <div>
-      <?php echo get_field('rule-two'); ?>
-    </div>
-    </section>
-    <section class="host-event-card">
-      <figure>
-        <img src="<?php echo get_field('rule-three-image'); ?>">
-      </figure>
-      <div>
-      <?php echo get_field('rule-three'); ?>
-    </div>
-    </section>
-    <section class="host-event-card">
-      <figure>
-        <img src="<?php echo get_field('rule-four-image'); ?>">
-      </figure>
-      <div>
-      <?php echo get_field('rule-four'); ?>
-    </div>
-    </section>
-  </section>
+  <?php get_template_part('templates/components/generic-four-cards-section'); ?>
+  <?php get_template_part('templates/components/generic-row'); ?>
