24960b749827e3de539f62f855c9793bfa5432e0
Corriger <rsmoraldo2@gmail.com>
2017-12-01 14:17:10 -0500

    php file updates

--- site/web/app/themes/sage/templates/template-host-event.php
+++ site/web/app/themes/sage/templates/template-host-event.php
@@ -3,5 +3,5 @@
  * Template Name: Host Event Template
  */
 ?>
-  <?php get_template_part('templates/components/generic-four-cards-section'); ?>
   <?php get_template_part('templates/components/generic-row'); ?>
+  <?php get_template_part('templates/components/generic-four-cards-section'); ?>
