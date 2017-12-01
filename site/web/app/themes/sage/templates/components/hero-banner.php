49f7fe7eca819f8f78e3531e8d7b07e1234d4c3f
Corriger <rsmoraldo2@gmail.com>
2017-12-01 17:28:16 -0500

    hero banner fixed

--- site/web/app/themes/sage/templates/components/hero-banner.php
+++ site/web/app/themes/sage/templates/components/hero-banner.php
@@ -1,8 +1,8 @@
 <?php if( have_rows('hero_banner') ): ?>
   <?php while ( have_rows('hero_banner') ) : the_row(); ?>
     <section class="hero-banner">
-      <img src="<?php the_sub_field('banner-image'); ?>"/>
-      <p><?php the_sub_field('banner-text'); ?></p>
+      <img src="<?php echo the_sub_field('banner_image'); ?>"/>
+      <h3><?php the_sub_field('banner_text'); ?></h3>
     </section>
  <?php endwhile; ?>
 <?php endif; ?>
