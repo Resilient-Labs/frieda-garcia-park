3d4464e8fd0a1e33757948d2a05f2a4784e37c4a
Corriger <rsmoraldo2@gmail.com>
2017-12-02 12:16:12 -0500

    hero banner php: div container

--- site/web/app/themes/sage/templates/components/hero-banner.php
+++ site/web/app/themes/sage/templates/components/hero-banner.php
@@ -2,7 +2,9 @@
   <?php while ( have_rows('hero_banner') ) : the_row(); ?>
     <section class="hero-banner">
       <img src="<?php echo the_sub_field('banner_image'); ?>"/>
-      <h3><?php the_sub_field('banner_text'); ?></h3>
+      <div class="hero-banner-content-wrapper">
+        <h3><?php the_sub_field('banner_text'); ?></h3>
+      </div>
     </section>
  <?php endwhile; ?>
 <?php endif; ?>
